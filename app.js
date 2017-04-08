873bec149ebc3103dbe7c79206fc54a87f6d6d34
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-08 03:26:39 +0200

    opdateret for hvert sekundt

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 let barometer = require('neo-barometer');
 
+var temperatureData;
+var pressureData;
+var altitudeData;
+
+setInterval(function(){
+    console.log('tempratur: ' + temperatureData + ' Kelvin');
+    console.log('tryk: ' + pressureData + ' Pascal');
+    console.log('højde: ' + altitudeData + ' Meter');
+},1000);
+
 // 1. Just temperature
 let temperature = new barometer.Temperature();
 temperature.on('data', function(data){
-    console.log('tempratur: ' + (data-273) + 'Kelvin');
+    temperature = data-273;
 });
 //temperature.on('error', console.error.bind(console));
 
 // 2. Just pressure
 let pressure = new barometer.Pressure();
 pressure.on('data', function(data){
-    console.log('tryk: ' + data + 'Pascal');
+    pressureData = data;
 });
 //pressure.on('error', console.error.bind(console));
 
 // 3. Just altitude
 let altitude = new barometer.Altitude();
 altitude.on('data', function(data){
-    console.log('højde' + data + 'meter');
+    altitude = data;
 });
 //altitude.on('error', console.error.bind(console));
 
