01e7294591ef1f7f006e3b509423843c0ca2e123
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-08 03:08:40 +0200

    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let barometer = require('neo-barometer');
 // 1. Just temperature
 let temperature = new barometer.Temperature();
 temperature.on('data', function(data){
-    console.log('temperatur' +  data - 273.15);
+    console.log('tempratur' + data);
 });
 //temperature.on('error', console.error.bind(console));
 
