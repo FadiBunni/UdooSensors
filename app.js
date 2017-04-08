8ea854a75a29c86135fb1cb98751de67b4fc1c6b
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-08 02:46:49 +0200

    nyeste

---
+++ app.js
@@ -0,0 +1,21 @@
+let barometer = require('neo-barometer');
+
+// 1. Just temperature
+let temperature = new barometer.Temperature();
+temperature.on('data', console.log.bind(console));
+temperature.on('error', console.error.bind(console));
+
+// 2. Just pressure
+let pressure = new barometer.Pressure();
+pressure.on('data', console.log.bind(console));
+pressure.on('error', console.error.bind(console));
+
+// 3. Just altitude
+let altitude = new barometer.Altitude();
+altitude.on('data', console.log.bind(console));
+altitude.on('error', console.error.bind(console));
+
+// 4. Compound (all values)
+let compound = new barometer.Compound();
+compound.on('data', console.log.bind(console));
+compound.on('error', console.error.bind(console));
